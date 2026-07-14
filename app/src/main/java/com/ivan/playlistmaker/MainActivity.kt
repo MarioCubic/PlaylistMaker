a5b7c73b2804790aab6efd481a9202dd131b0b96
Иван <vanyaer26@gmail.com>
2026-07-15 00:35:52 +0300

    First commit after create repo

--- app/src/main/java/com/ivan/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/ivan/playlistmaker/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.ivan.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,7 +11,30 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediaButton = findViewById<Button>(R.id.media_button)
+        val setButton = findViewById<Button>(R.id.set_button)
 
+
+        searchButton.setOnClickListener {
+            val searchIntent = Intent (this, SearchActivity::class.java)
+            startActivity(searchIntent)
+        }
+        mediaButton.setOnClickListener {
+            val mediaIntent = Intent (this, MediaActivity::class.java)
+            startActivity(mediaIntent)
+        }
+        setButton.setOnClickListener {
+            val setIntent = Intent (this, SettingsActivity::class.java)
+            startActivity(setIntent)
+        }
     }
 }
+

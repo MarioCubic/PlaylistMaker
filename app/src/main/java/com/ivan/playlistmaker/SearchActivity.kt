4d26e0fd88f865de43ed930edb0d0d8161fdf9eb
Иван <vanyaer26@gmail.com>
2026-07-28 12:15:12 +0300

    Сдача домашнего задания 9-го спринта

--- app/src/main/java/com/ivan/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/ivan/playlistmaker/SearchActivity.kt
@@ -1,12 +1,18 @@
 package com.ivan.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var searchField: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,55 @@ class SearchActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        searchField = findViewById(R.id.search_input)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        val backButton = findViewById<ImageView>(R.id.back_button)
+        backButton.setOnClickListener {
+            finish()
+        }
+        clearButton.setOnClickListener {
+            searchField.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+
+        }
+        searchField.addTextChangedListener(simpleTextWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_INPUT, searchField.toString())
+
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        searchField.setText(savedInstanceState.getString("SEARCH_TEXT"))
+    }
+
+    companion object {
+        const val SEARCH_INPUT = "SEARCH_INPUT"
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
         }
     }
 }
--- app/src/main/java/com/ivan/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/ivan/playlistmaker/SettingsActivity.kt
@@ -1,13 +1,19 @@
 package com.ivan.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.net.toUri
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("QueryPermissionsNeeded")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,10 +24,41 @@ class SettingsActivity : AppCompatActivity() {
             insets
         }
         val backButton = findViewById<ImageView>(R.id.back_button)
+        val shareButton = findViewById<LinearLayout>(R.id.share_app)
+        val supportButton = findViewById<LinearLayout>(R.id.support)
+        val termsButton = findViewById<LinearLayout>(R.id.terms)
 
         backButton.setOnClickListener {
             finish()
         }
+        shareButton.setOnClickListener {
+            val shareIntent = Intent (Intent.ACTION_SEND).apply {
+                type = "text/plain"
+                putExtra(Intent.EXTRA_TEXT,"https://practicum.yandex.ru/android-developer/?from=main_search_item")
+            }
+            startActivity(Intent.createChooser(shareIntent, "Поделиться"))
+        }
+        supportButton.setOnClickListener {
+            val adresses: Array <String> = arrayOf("mario.cubic@yandex.ru")
+            val themeOfMail = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val supportIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = "mailto:".toUri()
+                putExtra(Intent.EXTRA_EMAIL, adresses)
+                putExtra(Intent.EXTRA_SUBJECT, themeOfMail)
+                putExtra(Intent.EXTRA_TEXT, message)
+
+            }
+                startActivity(supportIntent)
+            }
+        termsButton.setOnClickListener {
+            val webpage: Uri = "https://yandex.ru/legal/practicum_offer/ru/".toUri()
+            val termsIntent = Intent(Intent.ACTION_VIEW, webpage)
+            startActivity(termsIntent)
+        }
+
+        }
+
+
     }
 
-}
